e9372d175ade2c08530df4a86db83cab9158ecf3
ghosw <fabiovital@live.com.pt>
2020-01-08 15:58:25 +0000

    Create notion-iniline-math-equations.js

---
+++ notion-iniline-math-equations.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name            Notion Inline Latex Math Equation
+// @namespace       https://github.com/Penguinlay/notion-inline-latex
+// @match           https://www.notion.so/*
+// @version         0.5
+// @description     Render Latex in notion
+// @require         https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js
+// @require         https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js
+// @grant           katexStyle
+// ==/UserScript==
+
+// Acknowledgement
+// This script was inspired by: https://github.com/evertheylen/notion-inline-math & https://github.com/Penguinlay/notion-inline-latex
+
+// right-padding for inline math mode
+katexStyle(`
+.katex {
+    padding-right: 0 !important;
+}
+`);
+
+let timeBetweenRenders = 500;
+
+function htmlToElement(str) {
+    var template = document.createElement('template');
+    str = str.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = str;
+    return template.content.firstChild;
+}
+
+// render inline LaTeX
+function renderInlineLaTeX() {
+  activeEl = document.activeElement;
+  activeEl.classList.add('do-not-render-katex-123456789');
+  renderMathInElement(document.body, {
+    delimiters: [
+      // LaTeX delimiters (uncomment/add as needed)
+      // { left: "$$" , right: "$$" , display: true  },
+      // { left: "\\[", right: "\\]", display: true  },
+      // { left: "\\(", right: "\\)", display: false },
+      { left: "$", right: "$", display: false }
+    ],
+    ignoredClasses: ['do-not-render-katex-123456789']
+  });
+  activeEl.classList.remove('do-not-render-katex-123456789');
+}
+
+katexLink = htmlToElement('<link href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.css" type="text/css" rel="stylesheet">');
+document.head.appendChild(katexLink);
+setInterval(renderInlineLaTeX, timeBetweenRenders);
